b29adb0ed9e9f1b5fee145d815f7252b752b66ee
Esther Mulwa <mulwa.esther@gmail.com>
2025-09-01 23:37:51 +0100

    Add VFIED branding and mobile support

--- dist/assets/index-9DD1HHrM.js
+++ dist/assets/index-9DD1HHrM.js
@@ -1,2 +1,2 @@
 function t(){import.meta.url,import("_").catch(()=>1),(async function*(){})().next()}export{t as __vite_legacy_guard};
-//# sourceMappingURL=index-B-xL6sAM.js.map
+//# sourceMappingURL=index-9DD1HHrM.js.map
